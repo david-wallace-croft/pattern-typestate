40fbf2c47d1233cc93c44fa2504e3e4f294ebaee
David Wallace Croft <david@CroftSoft.com>
2024-08-11 22:04:09 -0500

    removed get position

--- src/example4/typestate.rs
+++ src/example4/typestate.rs
@@ -1,9 +1,8 @@
 use super::ejected::EjectedState;
 use super::request::Request;
 use super::running::RunningState;
+use super::state_operator::StateOperator;
 use super::stopped::StoppedState;
-use crate::example4::state_operator::StateOperator;
-use crate::example4::state_trait::StateTrait;
 
 #[derive(Debug, PartialEq)]
 pub enum Typestate {
@@ -28,15 +27,5 @@ impl Typestate {
 impl Default for Typestate {
   fn default() -> Self {
     Typestate::Stopped(StateOperator::<StoppedState>::new(0))
-  }
-}
-
-impl StateTrait for Typestate {
-  fn get_position(&self) -> usize {
-    match self {
-      Typestate::Ejected(state_operator) => state_operator.get_position(),
-      Typestate::Running(state_operator) => state_operator.get_position(),
-      Typestate::Stopped(state_operator) => state_operator.get_position(),
-    }
   }
 }
