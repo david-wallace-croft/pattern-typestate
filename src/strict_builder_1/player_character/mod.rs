e4827d13648f38f9860b4927fcd1df146273a5f3
David Wallace Croft <david@CroftSoft.com>
2024-08-17 09:31:23 -0500

    strict builder module

--- src/strict_builder_1/player_character/mod.rs
+++ src/strict_builder_1/player_character/mod.rs
@@ -2,9 +2,10 @@ use super::armor::Armor;
 use super::character_class::CharacterClass;
 use super::spell::Spell;
 use super::weapon::Weapon;
-use super::wizard_weapon::WizardWeapon;
+use strict_builder::StrictBuilderPlayerCharacter;
 
-#[derive(Debug, PartialEq)]
+mod strict_builder;
+
 pub struct PlayerCharacter {
   armor: Armor,
   character_class: CharacterClass,
@@ -14,117 +15,13 @@ pub struct PlayerCharacter {
 
 impl PlayerCharacter {
   pub fn builder() -> StrictBuilderPlayerCharacter {
-    StrictBuilderPlayerCharacter {
-      player_character: PlayerCharacter {
-        armor: Armor::None,
-        character_class: CharacterClass::None,
-        spell: Spell::None,
-        weapon: Weapon::None,
-      },
-    }
-  }
-}
-
-pub struct StrictBuilderPlayerCharacter {
-  player_character: PlayerCharacter,
-}
-
-impl StrictBuilderPlayerCharacter {
-  pub fn warrior(mut self) -> StrictBuilderWarriorWeapon {
-    self
-      .player_character
-      .character_class = CharacterClass::Warrior;
-
-    StrictBuilderWarriorWeapon {
-      player_character: self.player_character,
-    }
-  }
-
-  pub fn wizard(mut self) -> StrictBuilderWizardWeapon {
-    self
-      .player_character
-      .character_class = CharacterClass::Wizard;
-
-    StrictBuilderWizardWeapon {
-      player_character: self.player_character,
-    }
-  }
-}
-
-pub struct StrictBuilderWarriorWeapon {
-  player_character: PlayerCharacter,
-}
-
-impl StrictBuilderWarriorWeapon {
-  pub fn weapon(
-    mut self,
-    weapon: Weapon,
-  ) -> StrictBuilderArmor {
-    self
-      .player_character
-      .weapon = weapon;
-
-    StrictBuilderArmor {
-      player_character: self.player_character,
-    }
-  }
-}
-
-pub struct StrictBuilderArmor {
-  player_character: PlayerCharacter,
-}
-
-impl StrictBuilderArmor {
-  pub fn armor(
-    mut self,
-    armor: Armor,
-  ) -> PlayerCharacter {
-    self
-      .player_character
-      .armor = armor;
-
-    self.player_character
-  }
-}
-
-pub struct StrictBuilderWizardWeapon {
-  player_character: PlayerCharacter,
-}
-
-impl StrictBuilderWizardWeapon {
-  pub fn weapon(
-    mut self,
-    wizard_weapon: WizardWeapon,
-  ) -> StrictBuilderSpell {
-    let weapon: Weapon = match wizard_weapon {
-      WizardWeapon::Dagger => Weapon::Dagger,
-      WizardWeapon::None => Weapon::None,
-      WizardWeapon::Staff => Weapon::Staff,
+    let player_character = PlayerCharacter {
+      armor: Armor::None,
+      character_class: CharacterClass::None,
+      spell: Spell::None,
+      weapon: Weapon::None,
     };
 
-    self
-      .player_character
-      .weapon = weapon;
-
-    StrictBuilderSpell {
-      player_character: self.player_character,
-    }
-  }
-}
-
-pub struct StrictBuilderSpell {
-  player_character: PlayerCharacter,
-}
-
-impl StrictBuilderSpell {
-  pub fn spell(
-    mut self,
-    spell: Spell,
-  ) -> PlayerCharacter {
-    self
-      .player_character
-      .spell = spell;
-
-    self.player_character
+    StrictBuilderPlayerCharacter::new(player_character)
   }
 }
---
+++ src/strict_builder_1/player_character/strict_builder.rs
@@ -0,0 +1,116 @@
+use super::super::armor::Armor;
+use super::super::character_class::CharacterClass;
+use super::super::player_character::PlayerCharacter;
+use super::super::spell::Spell;
+use super::super::weapon::Weapon;
+use super::super::wizard_weapon::WizardWeapon;
+
+pub struct StrictBuilderPlayerCharacter {
+  player_character: PlayerCharacter,
+}
+
+impl StrictBuilderPlayerCharacter {
+  pub(super) fn new(player_character: PlayerCharacter) -> Self {
+    Self {
+      player_character,
+    }
+  }
+
+  pub fn warrior(mut self) -> StrictBuilderWarriorWeapon {
+    self
+      .player_character
+      .character_class = CharacterClass::Warrior;
+
+    StrictBuilderWarriorWeapon {
+      player_character: self.player_character,
+    }
+  }
+
+  pub fn wizard(mut self) -> StrictBuilderWizardWeapon {
+    self
+      .player_character
+      .character_class = CharacterClass::Wizard;
+
+    StrictBuilderWizardWeapon {
+      player_character: self.player_character,
+    }
+  }
+}
+
+pub struct StrictBuilderWarriorWeapon {
+  player_character: PlayerCharacter,
+}
+
+impl StrictBuilderWarriorWeapon {
+  pub fn weapon(
+    mut self,
+    weapon: Weapon,
+  ) -> StrictBuilderArmor {
+    self
+      .player_character
+      .weapon = weapon;
+
+    StrictBuilderArmor {
+      player_character: self.player_character,
+    }
+  }
+}
+
+pub struct StrictBuilderArmor {
+  player_character: PlayerCharacter,
+}
+
+impl StrictBuilderArmor {
+  pub fn armor(
+    mut self,
+    armor: Armor,
+  ) -> PlayerCharacter {
+    self
+      .player_character
+      .armor = armor;
+
+    self.player_character
+  }
+}
+
+pub struct StrictBuilderWizardWeapon {
+  player_character: PlayerCharacter,
+}
+
+impl StrictBuilderWizardWeapon {
+  pub fn weapon(
+    mut self,
+    wizard_weapon: WizardWeapon,
+  ) -> StrictBuilderSpell {
+    let weapon: Weapon = match wizard_weapon {
+      WizardWeapon::Dagger => Weapon::Dagger,
+      WizardWeapon::None => Weapon::None,
+      WizardWeapon::Staff => Weapon::Staff,
+    };
+
+    self
+      .player_character
+      .weapon = weapon;
+
+    StrictBuilderSpell {
+      player_character: self.player_character,
+    }
+  }
+}
+
+pub struct StrictBuilderSpell {
+  player_character: PlayerCharacter,
+}
+
+impl StrictBuilderSpell {
+  pub fn spell(
+    mut self,
+    spell: Spell,
+  ) -> PlayerCharacter {
+    self
+      .player_character
+      .spell = spell;
+
+    self.player_character
+  }
+}
