e549c6ae13de23fd464deb86229062f5c9055879
David Wallace Croft <david@CroftSoft.com>
2024-09-02 13:59:24 -0500

    mem take

--- src/state_machine_0/player/state_machine/mod.rs
+++ src/state_machine_0/player/state_machine/mod.rs
@@ -1,5 +1,6 @@
 use self::typestate::Typestate;
 use super::event::Event;
+use std::mem;
 
 // The StateMachine submodules are private
 mod data;
@@ -12,23 +13,22 @@ mod typestate;
 #[cfg(test)]
 mod test;
 
+#[derive(Default)]
 pub struct StateMachine {
-  typestate_option: Option<Typestate>,
+  typestate: Typestate,
 }
 
 impl StateMachine {
   pub fn get_position(&self) -> usize {
-    match &self.typestate_option {
-      None => unreachable!(),
-      Some(typestate) => typestate.get_position(),
-    }
+    self
+      .typestate
+      .get_position()
   }
 
   pub fn get_state_name(&self) -> &'static str {
-    match &self.typestate_option {
-      None => unreachable!(),
-      Some(typestate) => typestate.get_state_name(),
-    }
+    self
+      .typestate
+      .get_state_name()
   }
 
   pub fn transit(
@@ -38,24 +38,8 @@ impl StateMachine {
     // Doing this directly will not compile; cannot move
     // self.typestate = self.typestate.transit(event);
 
-    let typestate_option = self
-      .typestate_option
-      .take();
+    let typestate: Typestate = mem::take(&mut self.typestate);
 
-    let typestate_old: Typestate = typestate_option.unwrap_or_default();
-
-    let typestate_new = typestate_old.transit(event);
-
-    self
-      .typestate_option
-      .replace(typestate_new);
-  }
-}
-
-impl Default for StateMachine {
-  fn default() -> Self {
-    Self {
-      typestate_option: Some(Typestate::default()),
-    }
+    self.typestate = typestate.transit(event);
   }
 }
