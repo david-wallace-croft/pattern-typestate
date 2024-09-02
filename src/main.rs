2d32a2c9025e51a1b24aaf1433f03887122a7624
David Wallace Croft <david@CroftSoft.com>
2024-09-02 08:03:11 -0500

    removed example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use pattern_typestate::{
   fluent_constructor_0, fluent_constructor_1, fluent_constructor_2,
-  fluent_constructor_3, state_machine_0, state_machine_1, state_machine_2,
+  fluent_constructor_3, state_machine_1, state_machine_2,
 };
 
 fn main() {
@@ -11,8 +11,6 @@ fn main() {
   fluent_constructor_2::example();
 
   fluent_constructor_3::example();
-
-  state_machine_0::example();
 
   state_machine_1::example();
 
--- src/state_machine_0/request.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug)]
-pub enum Request {
-  Eject,       // when stopped
-  Reset,       // when stopped
-  Run,         // when stopped
-  Skip(isize), // when running
-  Stop,        // when running
-}
